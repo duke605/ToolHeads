0f3a67d8597213618ea6a15889402ce29b9f1880
duke605 <duke6605@gmail.com>
2014-08-30 16:31:29 -0500

    Added Dependencies (oops)

--- src/main/java/duke605/ms/toolheads/lib/Reference.java
+++ src/main/java/duke605/ms/toolheads/lib/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
     public static final String MODID = "ToolHeads";
     public static final String NAME = "ToolHeads";
     public static final String VERSION = "@VERSION@";
-    public static final String DEPENDENCIES = "";
+    public static final String DEPENDENCIES = "required-after:DC1@[3.4.2.19,)";
     public static final String DOMAIN = "toolheads:";
 
 }
